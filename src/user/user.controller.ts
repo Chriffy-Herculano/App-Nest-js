3771c6be36ac34a67f148a7c551be389074992fd
Chriffy-Herculano <chriffy123@gmail.com>
2024-02-15 22:02:08 -0300

    Finalizando crud usando o ORM prisma

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -18,39 +18,31 @@ export class UserController {
     //Retorna todos
     @Get() 
     async list() {
-        return {users:[]};
+        //return {users:[]};
+        return this.userService.list();
     }
 
     //Retorna um específico
-    @Get(':id')
+    @Get(':id') //ParseIntPipe transforma em number
     async show(@Param('id', ParseIntPipe) id: number) {
-        return {user:{}, id}
+        //return {user:{}, id}
+        return this.userService.show(id);
     }
 
     //Alteração completa / precisa passar todos os se não ele vai ficar null
     @Put(':id')
-    async update(@Body() {name, email, password}: UpdatePutUserDTO, @Param('id', ParseIntPipe) id: number) {
-        return {
-            method: 'put',
-            name, email, password,
-            id
-        }
+    async update(@Body() data: UpdatePutUserDTO, @Param('id', ParseIntPipe) id: number) {
+        return this.userService.update(id, data);
     }
 
     //Alteração parcial
     @Patch(':id')
-    async updatePartial(@Body() {name, email, password}: UpdatePatchUserDTO, @Param('id', ParseIntPipe) id: number) {
-        return {
-            method: 'patch',
-            name, email, password,
-            id
-        }
+    async updatePartial(@Body() data: UpdatePatchUserDTO, @Param('id', ParseIntPipe) id: number) {
+        return this.userService.updatePartial(id, data);
     }
 
     @Delete(':id') //estou apenas pegando o id e transformando ele em number usando o ParseIntPipe
     async delete(@Param('id', ParseIntPipe) id: number) {
-        return {
-            id
-        };
+        return this.userService.delete(id);
     }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,6 +1,8 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
 import { CreateUserDTO } from "./dto/create-user.tdo";
 import { PrimaService } from "src/prisma/prisma.service";
+import { UpdatePutUserDTO } from "./dto/update-put-user.dto";
+import { UpdatePatchUserDTO } from "./dto/update-patch-user.dto";
 
 @Injectable()
 export class UserService {
@@ -21,6 +23,79 @@ export class UserService {
             }
         });
 
+    }
+
+    //Retornando todos
+    async list() {
+        return this.prisma.user.findMany();
+    }
+
+    //Retornando um especifico
+    async show(id: number) {
+        return this.prisma.user.findUnique({
+            where: {
+                id
+            }
+        })
+    }
+
+    async update(id: number, {email, name, password, birthAt}: UpdatePutUserDTO) {
+
+        await this.exists(id);
+
+        return this.prisma.user.update({
+            data:{email, name, password, birthAt: birthAt ? new Date(birthAt) : null},
+            where: {
+                id
+            }
+        })
+    }
+
+    async updatePartial(id: number, {email, name, password, birthAt}: UpdatePatchUserDTO) {
+
+        await this.exists(id);
+
+        const data: any = {};
+
+        if(birthAt) {
+            data.birthAt = new Date(birthAt);
+        }
+
+        if(email) {
+            data.email = email;
+        }
+
+        if(name) {
+            data.name = name;
+        }
+
+        if(password) {
+            data.password = password;
+        }
+
+        return this.prisma.user.update({
+            data,
+            where: {
+                id
+            }
+        })
+    }
+
+    async delete(id: number) {
+
+        await this.exists(id);
+
+        return this.prisma.user.delete({
+            where: {
+                id
+            }
+        });
+    }
+
+    async exists(id: number){
+        if(!(await this.show(id))){
+            throw new NotFoundException(`O usuário ${id} não existe!`);
+        }
     }
 
 }

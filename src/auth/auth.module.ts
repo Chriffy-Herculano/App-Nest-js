65ef39cdd3029a57a1feb452277306f3457ac5a9
Chriffy-Herculano <chriffy123@gmail.com>
2024-02-20 21:23:25 -0300

    Implementando segurança nas rotas

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,18 @@
 import { Module } from "@nestjs/common";
 import { JwtModule } from "@nestjs/jwt";
+import { AuthController } from "./auth.controller";
+import { UserModule } from "src/user/user.module";
+import { PrismaModule } from "src/prisma/prisma.module";
 
 @Module({
-    imports: [JwtModule.register({
-        secret: `e@r,@6P*W)c]kO2#dZ"}4?-2'rY@k$4=`
-    })]
+    imports: [
+        JwtModule.register({
+            secret: `e@r,@6P*W)c]kO2#dZ"}4?-2'rY@k$4=`
+        }),
+        UserModule,
+        PrismaModule,
+    ],
+    controllers: [AuthController]
 })
 
 export class AuthModule {
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,14 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, UnauthorizedException } from "@nestjs/common";
 import { JwtService } from "@nestjs/jwt";
+import { PrimaService } from "src/prisma/prisma.service";
 
 @Injectable()
 export class AuthService {
 
-    constructor(private readonly jwtService: JwtService) {}
+    constructor(
+        private readonly jwtService: JwtService, 
+        private readonly prisma: PrimaService
+    ) {}
 
     async createToken() {
         //return this.jwtService.sign();
@@ -13,4 +17,28 @@ export class AuthService {
     // async checkToken(token: string) {
     //     return this.jwtService.verify();
     // }
+
+    async login(email: string, password: string) {
+
+        const user = await this.prisma.user.findFirst({
+            where: {
+                email,
+                password
+            }
+        });
+
+        if(!user){
+            throw new UnauthorizedException('E-mail e/ou senha incorretos.');
+        }
+
+        return user;
+    }
+
+    async forget() {
+
+    }
+
+    async reset() {
+
+    }
 }
